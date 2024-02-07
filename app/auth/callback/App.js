4ea39f80f0eda923e2aa9b569673492db86b4be9
sanamarediya <sanamarediya@yahoo.com>
2024-02-07 12:05:49 -0600

    removed extra pages

--- app/auth/callback/App.js
+++
@@ -1,7 +0,0 @@
-import { route } from 'app/auth/callback/route.js';
-
-export const App = () => {
-  return (
-    <route />
-  );
-}
--- app/auth/callback/route.js
+++ app/auth/callback/route.js
@@ -2,7 +2,6 @@ import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
 import { NextRequest, NextResponse } from "next/server";
 import { cookies } from "next/headers";
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import { Landing } from 'app/Landing.jsx';
 
 export async function GET(request) {
     const requestUrl = new URL(request.url);
@@ -17,14 +16,3 @@ export async function GET(request) {
 }
 
 
-export const route = () => {
-    return (
-        <Router>
-            <Switch>
-                <Route path = "/">
-                    <Landing />
-                </Route>
-            </Switch>
-        </Router>
-    )
-}
