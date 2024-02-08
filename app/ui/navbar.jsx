b154705c0ef0efa4c464b013fc809906e87a60ed
Sarah Sotelo <saraheilene_sotelo@outlook.com>
2024-02-08 17:27:41 -0600

    Fixed colors of navbar

--- app/ui/navbar.jsx
+++ app/ui/navbar.jsx
@@ -1,23 +1,18 @@
 'use client'
 
 import {
-  HoverCard,
+  Avatar,
   Group,
   Button,
-  UnstyledButton,
-  Text,
-  SimpleGrid,
-  ThemeIcon,
-  Anchor,
   Divider,
   Center,
   Box,
   Burger,
   Drawer,
-  Collapse,
   ScrollArea,
   rem,
   useMantineTheme,
+  ActionIcon,
 } from '@mantine/core';
 import { useDisclosure } from '@mantine/hooks';
 import {
@@ -36,31 +31,33 @@ export default function Navbar() {
     <Box pb={120}>
       <header className={classes.header}>
         <Group justify="space-between" h="100%">
-        <IconBook
-          size={38}
-          stroke={1.5}
-          color="var(--mantine-color-blue-filled)"
-        />
+          <IconBook
+            size={38}
+            stroke={1.5}
+            color="white"
+          />
 
           <Group h="100%" gap={0} visibleFrom="sm">
-            <a href="/" className={classes.link}>
+            <a href="/" className={classes.link} style={{color:'white'}}>
               Home
             </a>
-            <a href="/studor/studygroup" className={classes.link}>
+            <a href="/studor/studygroup" className={classes.link} style={{color:'white'}}>
               Study Group
             </a>
-            <a href="/studor/tutoring" className={classes.link}>
+            <a href="/studor/tutoring" className={classes.link} style={{color:'white'}}>
               Tutoring
             </a>
           </Group>
 
           <Group visibleFrom="sm">
-            <Button variant="default">Log in</Button>
-            <Button>Sign up</Button>
+            <Button variant="outline" color="rgba(255, 255, 255, 1)" radius="xl">Log in</Button>
+            <ActionIcon variant="subtle" size="lg" color="rgba(255, 255, 255, 1)" radius="xl" aria-label="Profile">
+              <Avatar src={null} variant='transparent' alt="Profile" color="rgba(255, 255, 255, 1)"/>
+            </ActionIcon>
             <LightOrDarkMode />
           </Group>
 
-          <Burger opened={drawerOpened} onClick={toggleDrawer} hiddenFrom="sm" />
+          <Burger opened={drawerOpened} onClick={toggleDrawer} hiddenFrom="sm" color='white'/>
         </Group>
       </header>
 
@@ -72,9 +69,10 @@ export default function Navbar() {
         title="Navigation"
         hiddenFrom="sm"
         zIndex={1000000}
+        overlayColor={'#800000'}
       >
-        <ScrollArea h={`calc(100vh - ${rem(80)})`} mx="-md">
-          <Divider my="sm" />
+        <ScrollArea h={`calc(100vh - ${rem(80)})`} mx="-md" bg={'#800000'}>
+          <Divider my="sm" color="rgba(255, 255, 255, 1)"/>
 
           <a href="/" className={classes.link}>
             Home
@@ -86,16 +84,21 @@ export default function Navbar() {
             Tutoring
           </a>
 
-          <Divider my="sm" />
+          <Divider my="sm" color="rgba(255, 255, 255, 1)"/>
 
-          <Group justify="center" grow pb="xl" px="md">
-            <Button variant="default">Log in</Button>
-            <Button>Sign up</Button>
+          <Group justify="center" grow pb="xl" color="rgba(255, 255, 255, 1)" px="md">
+            <Button radius="xl" color="rgba(255, 255, 255, 1)" variant="outline">Log in</Button>
           </Group>
 
-          <Center>
+          <Group justify="center" grow pb="xl" px="md">
+            <ActionIcon variant="subtle" size="lg" color="rgba(255, 255, 255, 1)" radius="xl" aria-label="Profile">
+              <Avatar src={null} variant='transparent' alt="Profile" color="rgba(255, 255, 255, 1)"/>
+            </ActionIcon>
+          </Group>
+
+          <Group justify="center" grow pb="xl" px="md">
             <LightOrDarkMode />
-          </Center> 
+          </Group> 
         </ScrollArea>
       </Drawer>
     </Box>
