c25ab1328beec9b50daa8112614e22da8d8fdde9
Sarah Sotelo <saraheilene_sotelo@outlook.com>
2024-02-09 23:50:32 -0600

    Fixed validation on form for some values in Creating a Session page

--- app/studor/newstudygroupposting/page.jsx
+++ app/studor/newstudygroupposting/page.jsx
@@ -1,5 +1,5 @@
 'use client'
-import { Center, Group, MantineProvider, Stack, TextInput, CloseButton, Autocomplete, NumberInput, Button, Textarea, Space } from '@mantine/core'
+import { Center, Group, MantineProvider, Stack, TextInput, CloseButton, Autocomplete, NumberInput, Button, Textarea, Space, rem } from '@mantine/core'
 import { DateTimePicker } from '@mantine/dates';
 import { useState } from 'react';
 import { notifications } from '@mantine/notifications';
@@ -10,11 +10,11 @@ import { useForm } from '@mantine/form';
 export default function Page() {
   const [titleValue, setTitleValue] = useState('');
   const [locationValue, setLocationValue] = useState('');
-  const [dateValue, setDateValue] = useState('');
   var today = new Date();
   var dd = String(today.getDate());
   var mm = String(today.getMonth());
   var yyyy = today.getFullYear();
+  const dateTimeRegex = /^(Sun|Mon|Tue|Wed|Thu|Fri|Sat) (Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec) (0[1-9]|[1-2][0-9]|3[01]) \d{4} (0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]:[0-5][0-9] GMT[+-]\d{4} \([A-Za-z ]+\)$/;
 
   const departmentData = Array(100)
   .fill(0)
@@ -26,34 +26,43 @@ export default function Page() {
 
   const form = useForm({
     validateInputOnChange: true,
-    initialValues: { title: '', description: '', department: '', courseNumber:'', location:'', groupSize:1, dateAndTime:''},
+    initialValues: { title: '', description: '', department: '', courseNumber:'', location:'', groupSize:1, dateAndTime:today},
 
     validate: {
-      title: (value) => (value.length < 1 ? 'Title must have at least 1 character' : null),
-      department: (value) => ((value.length != 4 || /^[^a-zA-Z]*$/.test(value)) ? 'Invalid format of Department' : null),
-      courseNumber: (value) => ((value.length != 3 || /^[^\d]*$/.test(value)) ? 'Invalid format of Course Number' : null),
-      groupSize: (value) => ((value.length < 1 || value.length < 20) ? 'Invalid number in Group Size' : null),
+      title: (value) => (value.length < 2 ? 'Must have at least 2 characters' : null),
+      department: (value) => ((value.length != 4 || /^[^a-zA-Z]*$/.test(value)) ? 'Invalid Department' : null),
+      courseNumber: (value) => ((value.length != 3 || !(/^\d*$/.test(value))) ? 'Invalid Course Number' : null),
+      location: (value) => (value.length < 2 ? 'Invalid Location' : null),
+      groupSize: (value) => ((value < 1 || value < 20) ? 'Invalid Group Size' : null),
+      dateAndTime: (value) => (dateTimeRegex.test(value) ? null : 'Invalid date and time'),
     },
   });
 
-  // const handleError = (errors) => {
-  //   if (errors.title) {
-  //     notifications.show({ message: 'Please fill title field', color: 'red' });
-  //   } else if (errors.department) {
-  //     notifications.show({ message: 'Please provide a valid email', color: 'red' });
-  //   }
-  //   else {
-  //     notifications.show({
-  //       withBorder: true,
-  //       title: 'Session Created!',
-  //       message: 'Your new session was successfully created',
-  //       color: 'green',
-  //       icon: <IconCircleCheck />,
-  //       withCloseButton: true,
-  //       loading: false,
-  //     })
-  //   }
-  // };
+  const handleSubmit = (event) => {
+    event.preventDefault(); // Prevent default form submission
+  
+    const checkingValues = event.target;
+    if (!checkingValues.checkValidity()) {
+      return console.log('Form is invalid');
+    }
+
+    const values = form.values; // Get form values
+    console.log('Form values:', values); // Log form values
+  
+    notifications.show({
+      withBorder: true,
+      color: "green",
+      radius: "md",
+      icon: <IconCircleCheck style={{ width: rem(18), height: rem(18) }} />,
+      title: 'New Session Created! Redirecting...',
+      message: "Now redirecting to Landing Page",
+    });
+    
+    // Redirect to the new page after a short delay
+    setTimeout(() => {
+      window.location.href = '/';
+    }, 5000);
+  };
 
   return (
     <MantineProvider>
@@ -63,7 +72,7 @@ export default function Page() {
 
       <Center mx={25}>
         <Stack>
-          <form onSubmit={form.onSubmit(console.log)}>
+          <form onSubmit={handleSubmit}>
             <TextInput
               label="Title"
               placeholder="Title of Session"
@@ -83,9 +92,10 @@ export default function Page() {
             <Textarea
               label="Description"
               placeholder="Write a description of the session here"
+              mt={15}
               {...form.getInputProps('description')}
             />
-            <Group grow>
+            <Group grow mt={15}>
               <Autocomplete
                 label="Department"
                 placeholder="Enter Four Letters"
@@ -107,6 +117,7 @@ export default function Page() {
               label="Location"
               placeholder="Location of Session"
               value={locationValue}
+              mt={15}
               onChange={(event) => setLocationValue(event.currentTarget.value)}
               rightSectionPointerEvents="all"
               rightSection={
@@ -119,11 +130,11 @@ export default function Page() {
               required
               {...form.getInputProps('location')}
             />
-            <Group grow>
+            <Group grow mt={15}>
               <NumberInput
                   label="Group Size"
                   placeholder="Enter a Value 1-20"
-                  description="Don't include yourself."
+                  description="Don't include yourself"
                   min={1}
                   max={20}
                   required
@@ -131,27 +142,26 @@ export default function Page() {
                 />
               <DateTimePicker
                 allowDeselect
-                clearable
                 valueFormat="DD MMM YYYY hh:mm A"
                 label="Date and Time"
-                description="Include if AM or PM."
+                description="Include if AM or PM"
                 placeholder="Select a Date and Time"
-                value={dateValue}
-                onChange={setDateValue}
                 minDate={new Date(yyyy, mm, dd)}
                 required
                 {...form.getInputProps('dateAndTime')}
               />
             </Group>
-            <Button 
-              type='submit'
-              mt="md" 
-              variant="filled" 
-              color='#800000' 
-              radius="xl" 
-              >
-              Post Session
-            </Button>
+            <Stack align="center" mt={20}>
+              <Button 
+                type='submit'
+                mt="md" 
+                variant="filled" 
+                color='#800000' 
+                radius="xl"
+                >
+                Post Session
+              </Button>
+            </Stack>
           </form>
         </Stack>
       </Center>
