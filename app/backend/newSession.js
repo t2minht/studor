c9cce4fc92c1b0e2a83cc878fd4d84107d4a7369
Shawn Mathen <sgmathen@gmail.com>
2024-02-15 16:18:26 -0600

    retrieving users and putting them on landing page

--- app/backend/newSession.js
+++ app/backend/newSession.js
@@ -25,10 +25,43 @@ export async function submitStudyGroupSessionData(data) {
       host_user_id: user.id
     }
   ]).select();
-  console.log('error', error);
-  console.log('returned_data', returned_data);
+  // console.log('error', error);
+  // console.log('returned_data', returned_data);
 
 
+}
+
+export async function retrieveExistingSessions() {
+
+  const supabase = createServerActionClient({ cookies })
+
+  const currentDateTime = new Date();
+  const currentDate = currentDateTime.toISOString().split('T')[0];
+  const currentTime = currentDateTime.toTimeString().split(' ')[0];
+
+
+
+  try {
+    const { data, error } = await supabase
+      .from('study_sessions')
+      .select()
+      .gte('date', currentDate)
+      .gte('end_time', currentTime)
+      .order('date')
+      .order('end_time');
+
+    if (error) {
+      console.error(error);
+      throw new Error("Error fetching study sessions");
+    }
+
+    return data;
+
+  }
+  catch (error) {
+    console.log('error', error);
+    throw error;
+  }
 }
 
 // export function submitTutorSessionData(data) {
