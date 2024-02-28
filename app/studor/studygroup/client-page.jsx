aa840c004cd7549ba728168d0effe9ac90c8af4b
sanamarediya <sanamarediya@yahoo.com>
2024-02-28 09:53:25 -0600

    merge edits with testing

--- app/studor/studygroup/client-page.jsx
+++ app/studor/studygroup/client-page.jsx
@@ -18,21 +18,41 @@ import { useDisclosure } from "@mantine/hooks";
 import Modalview from "../../ui/modalview";
 import { useViewportSize } from "@mantine/hooks";
 import { useState } from "react";
-import JoinSessionButton from "./join-session-button";
+import { joinSession } from "@/app/backend/study-session-backend";
 
 export default function ClientPage(data) {
     const [opened, { open, close }] = useDisclosure(false);
     const { height, width } = useViewportSize();
     const [checked, setChecked] = useState(true);
 
-    if (data.study_sessions === null) {
-      return (
+    const [study_sessions, setStudySessions] = useState(data.study_sessions);
 
-        (
-          <Group>
-          <Text>Nothing to see here</Text>
-          </Group>
-          )
+    const joinHandler = async (session) => {
+
+        await joinSession(data = { session });
+        const updatedSessions = study_sessions.filter((item) => item.id !== session.id);
+        setStudySessions(updatedSessions);
+
+    }
+
+    const handleRemoveSession = (session) => {
+        // const updatedSessions = study_sessions.filter((item) => item.id !== session.id);
+        console.log('howdy')
+        // Log the updated study_sessions
+        // console.log("Updated study_sessions:", updatedSessions);
+
+        // // Update the state with the filtered sessions
+        // setStudySessions(updatedSessions);
+    }
+
+    if (study_sessions === null) {
+        return (
+
+            (
+                <Group>
+                    <Text>Nothing to see here</Text>
+                </Group>
+            )
         )
     }
 
@@ -91,36 +111,46 @@ export default function ClientPage(data) {
                     <Group miw={200}>
                         <ScrollArea h={height - 180}>
                             <Group>
-                                {data.study_sessions.map((session) => (
+                                {study_sessions
+                                    .filter((session) => session.current_group_size < session.max_group_size)
+                                    .map((session) => (
+                                        <Group p={30} key={session.topic}>
+                                            <Stack>
+                                                <Avatar size={100} />
+                                            </Stack>
+                                            <Stack>
+                                                <Stack>
+                                                    <Text fw={700} size="xl">
+                                                        {session.topic}
+                                                    </Text>
+                                                    <Text mt={-10} fw={700}>
+                                                        Class: {session.department + ' ' + session.course_number + (session.section ? ' - ' + session.section : '')}
+                                                    </Text>
+                                                    <Text mt={-15}>Location: {session.location}</Text>
+                                                    <Text mt={-15}>Date: {session.date}</Text>
+                                                    <Text mt={-15}>Time: {session.start_time} - {session.end_time}</Text>
+                                                    <Text mt={-15}>Available: {session.current_group_size} / {session.max_group_size} </Text>
+                                                </Stack>
+                                                <Group align="center">
+                                                    {/* <JoinSessionButton session={session} onClick={() => handleRemoveSession(session)} /> */}
+                                                    <Button
+                                                        variant="filled"
+                                                        size="sm"
+                                                        color="#009020"
+                                                        radius="xl"
+                                                        onClick={() => joinHandler(session)}
+                                                    >
+                                                        Join
+                                                    </Button>
+                                                    <Modalview current={session}/>
+                                                </Group>
+                                            </Stack>
+                                        </Group>
 
-                              <Group p={30} key={session.topic}>
-                              <Stack>
-                                  <Avatar size={100} />
-                              </Stack>
-                              <Stack>
-                                  <Stack>
-                                      <Text fw={700} size="xl">
-                                        {session.topic}
-                                      </Text>
-                                      <Text mt={-10} fw={700}>
-                                          Class: {session.department + ' ' + session.course_number + (session.section ? ' - ' + session.section : '')}
-                                      </Text>
-                                      <Text mt={-15}>Location: {session.location}</Text>
-                                      <Text mt={-15}>Date: {session.date}</Text>
-                                      <Text mt={-15}>Time: {session.start_time} - {session.end_time}</Text>
-                                      <Text mt={-15}>Available: {session.current_group_size} / {session.max_group_size} </Text>
-                                  </Stack>
-                                  <Group align="center">
-                                      <JoinSessionButton session_id={session.id} />
-                                      <Modalview current={session} />
-                                  </Group>
-                              </Stack>
-                              </Group>
 
 
 
-
-                                ))}
+                                    ))}
                             </Group>
                         </ScrollArea>
                     </Group>
