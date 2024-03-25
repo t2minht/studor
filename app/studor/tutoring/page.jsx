7935cc790b4288b015912a576ff96621d519eca6
Sarah Sotelo <saraheilene_sotelo@outlook.com>
2024-03-25 12:34:03 -0500

    Fixed error on tutoring

--- app/studor/tutoring/page.jsx
+++ app/studor/tutoring/page.jsx
@@ -6,21 +6,16 @@ import { retrieveProfileStudySession} from "@/app/backend/study-session-backend"
 import { retrieveUserEvents } from "@/app/backend/calendar-backend";
 
 export default async function Page() {
+
   const tutor_sessions = await getExistingNotJoinedSessions();
-  const hosted_study_sessions = await getHostedStudySessions();
-  const joined_study_sessions = await getJoinedStudySessions();
-  const departments = await getDepartmentNames();
-  const departmentsAndNull = [''].concat(departments);
   
-
-  const study_sessions = {};
-  study_sessions.hosted = hosted_study_sessions;
-  study_sessions.joined = joined_study_sessions;
-
   const fetchedEvents = await retrieveUserEvents();
   const fetchedStudySessions = await retrieveProfileStudySession();
   const fetchedTutorSessions = await retrieveProfileTutoringSessions();
 
+  const departments = await getDepartmentNames();
+  const departmentsAndNull = [''].concat(departments);
+  
   return (
     <MantineProvider>
       <ClientPage tutor_sessions={tutor_sessions} events = {fetchedEvents} all_study_sessions = {fetchedStudySessions} all_tutoring = {fetchedTutorSessions} departments={departmentsAndNull}></ClientPage>
