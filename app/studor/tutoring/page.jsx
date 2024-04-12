e4035cf8285f228d0e9a4f65b91aa2b436eff67f
t2minht <tuongtran0240@gmail.com>
2024-04-12 17:01:07 -0500

    oopsies forgot to push the color things

--- app/studor/tutoring/page.jsx
+++ app/studor/tutoring/page.jsx
@@ -3,7 +3,7 @@ import { MantineProvider } from "@mantine/core";
 import ClientPage from "./client-page";
 import { getDepartmentNames, getExistingNotJoinedSessions, retrieveProfileTutoringSessions } from "@/app/backend/tutoring-backend";
 import { retrieveProfileStudySession} from "@/app/backend/study-session-backend";
-import { retrieveUserEvents } from "@/app/backend/calendar-backend";
+import { retrieveUserEvents, getColorPref } from "@/app/backend/calendar-backend";
 
 export default async function Page() {
 
@@ -12,13 +12,14 @@ export default async function Page() {
   const fetchedEvents = await retrieveUserEvents();
   const fetchedStudySessions = await retrieveProfileStudySession();
   const fetchedTutorSessions = await retrieveProfileTutoringSessions();
+  const fetchedColors = await getColorPref();
 
   const departments = await getDepartmentNames();
   const departmentsAndNull = [''].concat(departments);
   
   return (
     <MantineProvider>
-      <ClientPage tutor_sessions={tutor_sessions} events = {fetchedEvents} all_study_sessions = {fetchedStudySessions} all_tutoring = {fetchedTutorSessions} departments={departmentsAndNull}></ClientPage>
+      <ClientPage tutor_sessions={tutor_sessions} events = {fetchedEvents} all_study_sessions = {fetchedStudySessions} all_tutoring = {fetchedTutorSessions} departments={departmentsAndNull} colors={fetchedColors}></ClientPage>
     </MantineProvider>
   )
 }
