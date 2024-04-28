9d30fe7558ea02034b837b9239975fb027669bc8
t2minht <tuongtran0240@gmail.com>
2024-04-28 15:45:49 -0500

    calendar.jsx code documented

--- app/auth/callback/route.js
+++ app/auth/callback/route.js
@@ -2,12 +2,12 @@ import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
 import { NextResponse } from "next/server";
 import { cookies } from "next/headers";
 
-export async function GET(request) {
+export async function GET(request) {            
     const requestUrl = new URL(request.url);
     const code = requestUrl.searchParams.get("code");
 
     if (code) {
-        const supabase = createRouteHandlerClient({ cookies });
+        const supabase = createRouteHandlerClient({ cookies }); 
         await supabase.auth.exchangeCodeForSession(code)
     }
 
